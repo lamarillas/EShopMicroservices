0ffe8312acf6022788801530216458656fc5faa3
leo amarillas <lamarillas@vertex.mx>
2026-03-27 17:22:41 -0700

    Parcial Product By Id  1/2

--- src/Services/Catalog/Catalog.API/Products/GetProducts/GetProductsHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/GetProducts/GetProductsHandler.cs
@@ -4,10 +4,7 @@ namespace Catalog.API.Products.GetProducts;
 public record GetProductsQuery() : IQuery<GetProductsResult>;
 public record GetProductsResult(IEnumerable<Product> Products);
 
-internal class GetProductsQueryHandler(
-    IDocumentSession session, 
-    ILogger<GetProductsQueryHandler> logger)
-    : IQueryHandler<GetProductsQuery, GetProductsResult>
+internal class GetProductsQueryHandler(IDocumentSession session, ILogger<GetProductsQueryHandler> logger) : IQueryHandler<GetProductsQuery, GetProductsResult>
 {
     public async Task<GetProductsResult> Handle(GetProductsQuery query, CancellationToken cancellationToken)
     {
