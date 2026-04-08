47a52fac35e7dde9c3d9409ee7166a5ed3656133
leo amarillas <lamarillas@vertex.mx>
2026-04-08 14:53:02 -0700

    Develop and test ProductByCategory

--- src/Services/Catalog/Catalog.API/Products/GetProductById/GetProductByIdEndpoint.cs
+++ src/Services/Catalog/Catalog.API/Products/GetProductById/GetProductByIdEndpoint.cs
@@ -1,5 +1,23 @@
-﻿namespace Catalog.API.Products.GetProductById;
+﻿
+namespace Catalog.API.Products.GetProductById;
 
-public class GetProductByIdEndpoint
+public record GetProductByIdResponse(Product Product);
+
+
+public class GetProductByIdEndpoint : ICarterModule
 {
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.Map("/products/{id}", async (Guid id, ISender sender) => {
+            var result = await sender.Send(new GetProductByIdQuery(id));
+
+            var response = result.Adapt<GetProductByIdResponse>();
+
+            return Results.Ok(response);
+        })
+        .WithName("GetProductById")
+        .Produces<GetProductByIdResponse>(StatusCodes.Status200OK)
+        .WithSummary("Get Product By Id")
+        .WithDescription("Get Product By Id");
+    }
 }
