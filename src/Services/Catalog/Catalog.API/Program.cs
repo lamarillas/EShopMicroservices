d31c3e1911f4b28d0046582b359f41643fd6a991
leo amarillas <lamarillas@vertex.mx>
2026-04-09 16:09:30 -0700

    Use Exception Handler Lamda

--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -1,4 +1,6 @@
 using BuildingBlocks.Behaviors;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -29,5 +31,34 @@ var app = builder.Build();
 
 // Configure the HTTP pipeline
 app.MapCarter();
+
+// Exception Handler Lambda 
+app.UseExceptionHandler(exceptionHanlderApp =>
+{
+    exceptionHanlderApp.Run(async context =>
+    {
+        var exception = context.Features.Get<IExceptionHandlerFeature>()?.Error;
+        if(exception is null)
+        {
+            return;
+        }
+
+        var problemDetails = new ProblemDetails
+        {
+            Title = exception.Message,
+            Status = StatusCodes.Status500InternalServerError,
+            Detail = exception.StackTrace
+        };
+
+        var logger = context.RequestServices.GetRequiredService<ILogger<Program>>();
+        logger.LogError(exception, exception.Message);
+
+        context.Response.StatusCode = StatusCodes.Status500InternalServerError;
+        context.Response.ContentType = "application/problem+json";
+
+        await context.Response.WriteAsJsonAsync(problemDetails);
+
+    });
+});
 
 app.Run();
