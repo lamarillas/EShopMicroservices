6dab2655be6f2d9a10fb397e2c99dc1e06b72ca5
leo amarillas <lamarillas@vertex.mx>
2026-04-13 17:38:06 -0700

    Register Custom Exception Handling with AddExceptionHandler

--- src/Services/Catalog/Catalog.API/GlobalUsing.cs
+++ src/Services/Catalog/Catalog.API/GlobalUsing.cs
@@ -6,3 +6,5 @@ global using Mapster;
 global using Marten;
 global using MediatR;
 global using FluentValidation;
+global using BuildingBlocks.Behaviors;
+global using BuildingBlocks.Exceptions.Handler;
--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -1,10 +1,5 @@
-using BuildingBlocks.Behaviors;
-using Microsoft.AspNetCore.Diagnostics;
-using Microsoft.AspNetCore.Mvc;
 
 var builder = WebApplication.CreateBuilder(args);
-
-
 
 
 // Add services to the container.
@@ -25,40 +20,13 @@ builder.Services.AddMarten(opt =>
 }).UseLightweightSessions();
 
 
-
+builder.Services.AddExceptionHandler<CustomExceptionHandler>();
 
 var app = builder.Build();
 
 // Configure the HTTP pipeline
 app.MapCarter();
 
-// Exception Handler Lambda 
-app.UseExceptionHandler(exceptionHanlderApp =>
-{
-    exceptionHanlderApp.Run(async context =>
-    {
-        var exception = context.Features.Get<IExceptionHandlerFeature>()?.Error;
-        if (exception is null)
-        {
-            return;
-        }
-
-        var problemDetails = new ProblemDetails
-        {
-            Title = exception.Message,
-            Status = StatusCodes.Status500InternalServerError,
-            Detail = exception.StackTrace
-        };
-
-        var logger = context.RequestServices.GetRequiredService<ILogger<Program>>();
-        logger.LogError(exception, exception.Message);
-
-        context.Response.StatusCode = StatusCodes.Status500InternalServerError;
-        context.Response.ContentType = "application/problem+json";
-
-        await context.Response.WriteAsJsonAsync(problemDetails);
-
-    });
-});
+app.UseExceptionHandler(options => { });
 
 app.Run();
